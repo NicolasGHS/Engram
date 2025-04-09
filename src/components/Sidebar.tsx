bb4f310f6e87f2b20541681e8710735bebdf5ab7
NicolasGHS <60781933+NicolasGHS@users.noreply.github.com>
2025-04-09 20:52:50 +0200

    sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,40 @@
-import { useState } from "react";
-import { Button } from "@chakra-ui/react";
+import { useState } from 'react';
+import {
+    Box,
+    Button,
+    Drawer,
+    DrawerHeader,
+    DrawerBody,
+    DrawerContent,
+    VStack,
+  } from '@chakra-ui/react'
 
-const Sidebar = () => {
-    const [isSidebarVisible, setIsSidebarVisible] = useState(true);
+
+  const Sidebar = () => {
+    const [isOpen, setIsOpen] = useState(true);
 
     return (
-        <>
-            <p className="">Sidebar</p>
-            <Button>Test button</Button>
-        </>
-    );  
+        <Drawer.Root placement={"start"} open={isOpen} size={"xs"} onOpenChange={(e) => setIsOpen(e.open)}>
+            <Drawer.Backdrop />
+            <Drawer.Trigger asChild>
+                <Button variant="outline" size="sm">
+                    Close
+                </Button>
+            </Drawer.Trigger>
+            <Drawer.Positioner>
+                <Drawer.Content>
+                <Drawer.CloseTrigger />
+                <Drawer.Header>
+                    <Drawer.Title />
+                </Drawer.Header>
+                <Drawer.Body>
+                    <p>Notebooks</p>
+                </Drawer.Body>
+                <Drawer.Footer />
+                </Drawer.Content>
+            </Drawer.Positioner>
+        </Drawer.Root>
+    )
 }
 
 export default Sidebar;
